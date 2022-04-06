291f9cb264398effcc41ef852f8079fc9f97f6f7
amerkadicE <amer.kadic@endava.com>
2022-04-06 08:30:27 +0200

    Fix for randomly resets of updated values in network edit form
    Code refactor
    
    Fix test

--- ui/pages/settings/networks-tab/networks-form/networks-form.js
+++ ui/pages/settings/networks-tab/networks-form/networks-form.js
@@ -5,6 +5,7 @@ import PropTypes from 'prop-types';
 import validUrl from 'valid-url';
 import log from 'loglevel';
 import classnames from 'classnames';
+import { isEqual } from 'lodash';
 import { useI18nContext } from '../../../../hooks/useI18nContext';
 import {
   isPrefixedFormattedHexString,
@@ -87,6 +88,8 @@ const NetworksForm = ({
   const [errors, setErrors] = useState({});
   const [warnings, setWarnings] = useState({});
   const [isSubmitting, setIsSubmitting] = useState(false);
+  const [isEditing, setIsEditing] = useState(Boolean(addNewNetwork));
+  const [previousNetwork, setPreviousNetwork] = useState(selectedNetwork);
 
   const resetForm = useCallback(() => {
     setNetworkName(selectedNetworkName || '');
@@ -97,6 +100,7 @@ const NetworksForm = ({
     setErrors({});
     setWarnings({});
     setIsSubmitting(false);
+    setPreviousNetwork(selectedNetwork);
   }, [selectedNetwork, selectedNetworkName]);
 
   const stateIsUnchanged = () => {
@@ -132,11 +136,12 @@ const NetworksForm = ({
       setErrors({});
       setIsSubmitting(false);
     } else if (
-      prevNetworkName.current !== selectedNetworkName ||
-      prevRpcUrl.current !== selectedNetwork.rpcUrl ||
-      prevChainId.current !== selectedNetwork.chainId ||
-      prevTicker.current !== selectedNetwork.ticker ||
-      prevBlockExplorerUrl.current !== selectedNetwork.blockExplorerUrl
+      (prevNetworkName.current !== selectedNetworkName ||
+        prevRpcUrl.current !== selectedNetwork.rpcUrl ||
+        prevChainId.current !== selectedNetwork.chainId ||
+        prevTicker.current !== selectedNetwork.ticker ||
+        prevBlockExplorerUrl.current !== selectedNetwork.blockExplorerUrl) &&
+      (!isEditing || !isEqual(selectedNetwork, previousNetwork))
     ) {
       resetForm(selectedNetwork);
     }
@@ -144,6 +149,7 @@ const NetworksForm = ({
     selectedNetwork,
     selectedNetworkName,
     addNewNetwork,
+    previousNetwork,
     setNetworkName,
     setRpcUrl,
     setChainId,
@@ -152,6 +158,7 @@ const NetworksForm = ({
     setErrors,
     setIsSubmitting,
     resetForm,
+    isEditing,
   ]);
 
   useEffect(() => {
@@ -466,6 +473,7 @@ const NetworksForm = ({
   ]);
 
   const onSubmit = async () => {
+    setIsEditing(false);
     setIsSubmitting(true);
     try {
       const formChainId = chainId.trim().toLowerCase();
@@ -510,11 +518,13 @@ const NetworksForm = ({
       dispatch(setSelectedSettingsRpcUrl(''));
       history.push(NETWORKS_ROUTE);
     } else {
+      setIsEditing(false);
       resetForm();
     }
   };
 
   const onDelete = () => {
+    setIsEditing(false);
     dispatch(
       showModal({
         name: 'CONFIRM_DELETE_NETWORK',
@@ -563,21 +573,30 @@ const NetworksForm = ({
         <FormField
           autoFocus
           error={errors.networkName?.msg || ''}
-          onChange={setNetworkName}
+          onChange={(value) => {
+            setIsEditing(true);
+            setNetworkName(value);
+          }}
           titleText={t('networkName')}
           value={networkName}
           disabled={viewOnly}
         />
         <FormField
           error={errors.rpcUrl?.msg || ''}
-          onChange={setRpcUrl}
+          onChange={(value) => {
+            setIsEditing(true);
+            setRpcUrl(value);
+          }}
           titleText={t('rpcUrl')}
           value={rpcUrl}
           disabled={viewOnly}
         />
         <FormField
           warning={warnings.chainId?.msg || ''}
-          onChange={setChainId}
+          onChange={(value) => {
+            setIsEditing(true);
+            setChainId(value);
+          }}
           titleText={t('chainId')}
           value={chainId}
           disabled={viewOnly}
@@ -585,14 +604,20 @@ const NetworksForm = ({
         />
         <FormField
           warning={warnings.ticker?.msg || ''}
-          onChange={setTicker}
+          onChange={(value) => {
+            setIsEditing(true);
+            setTicker(value);
+          }}
           titleText={t('currencySymbol')}
           value={ticker}
           disabled={viewOnly}
         />
         <FormField
           error={errors.blockExplorerUrl?.msg || ''}
-          onChange={setBlockExplorerUrl}
+          onChange={(value) => {
+            setIsEditing(true);
+            setBlockExplorerUrl(value);
+          }}
           titleText={t('blockExplorerUrl')}
           titleUnit={t('optionalWithParanthesis')}
           value={blockExplorerUrl}

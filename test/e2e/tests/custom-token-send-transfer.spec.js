7e0f1976476a6d6966574866b9fe3b7485cf296c
mirjanaKukic <mirjana.kukic@endava.com>
2022-06-02 16:34:38 +0200

    transfer token without specifying gas

--- test/e2e/tests/custom-token-send-transfer.spec.js
+++ test/e2e/tests/custom-token-send-transfer.spec.js
@@ -320,3 +320,95 @@ describe('Send a custom token from dapp', function () {
     );
   });
 });
+
+describe('Transfers a custom token from dapp when no gas value is specified', function () {
+  const ganacheOptions = {
+    accounts: [
+      {
+        secretKey:
+          '0x7C9529A67102755B7E6102D6D950AC5D5863C98713805CEC576B945B15B71EAC',
+        balance: convertToHexValue(25000000000000000000),
+      },
+    ],
+  };
+  it('transfers an already created token, without specifying gas, submits the transaction, finds the transaction in the transactions list', async function () {
+    await withFixtures(
+      {
+        dapp: true,
+        fixtures: 'connected-state',
+        ganacheOptions,
+        title: this.test.title,
+      },
+      async ({ driver }) => {
+        await driver.navigate();
+        await driver.fill('#password', 'correct horse battery staple');
+        await driver.press('#password', driver.Key.ENTER);
+
+        // create token
+        await driver.openNewPage(`http://127.0.0.1:8080/`);
+        await driver.waitForSelector({ text: 'Create Token', tag: 'button' });
+        await driver.clickElement({ text: 'Create Token', tag: 'button' });
+
+        let windowHandles = await getWindowHandles(driver, 3);
+        await driver.switchToWindow(windowHandles.popup);
+
+        await driver.clickElement({ text: 'Confirm', tag: 'button' });
+
+        await driver.switchToWindow(windowHandles.dapp);
+
+        await driver.waitForSelector({
+          css: '#tokenAddress',
+          text: '0x',
+        });
+
+        await driver.clickElement({
+          text: 'Add Token to Wallet',
+          tag: 'button',
+        });
+
+        windowHandles = await getWindowHandles(driver, 3);
+        await driver.switchToWindow(windowHandles.popup);
+        await driver.clickElement({ text: 'Add Token', tag: 'button' });
+
+        await driver.switchToWindow(windowHandles.dapp);
+
+        await driver.clickElement({
+          text: 'Transfer Tokens Without Gas',
+          tag: 'button',
+        });
+        windowHandles = await getWindowHandles(driver, 3);
+        await driver.switchToWindow(windowHandles.popup);
+        await driver.delay(3000);
+        await driver.clickElement({ text: 'Confirm', tag: 'button' });
+        await driver.switchToWindow(windowHandles.extension);
+        await driver.clickElement({ tag: 'button', text: 'Activity' });
+        await driver.wait(async () => {
+          const pendingTxes = await driver.findElements(
+            '.transaction-list__pending-transactions .transaction-list-item',
+          );
+          return pendingTxes.length === 1;
+        }, 10000);
+
+        await driver.waitForSelector({
+          css: '.transaction-list-item__primary-currency',
+          text: '-1.5 TST',
+        });
+        await driver.clickElement('.transaction-list-item');
+
+        await driver.waitForSelector({
+          // Select the heading of the first transaction list item in the
+          // completed transaction list with text matching Send TST
+          css:
+            '.transaction-list__completed-transactions .transaction-list-item:first-child .list-item__heading',
+          text: 'Send TST',
+        });
+
+        await driver.waitForSelector({
+          css:
+            '.transaction-list__completed-transactions .transaction-list-item:first-child .transaction-list-item__primary-currency',
+          text: '-1.5 TST',
+        });
+      },
+    );
+  });
+});
